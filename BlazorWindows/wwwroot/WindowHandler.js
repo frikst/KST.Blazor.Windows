5e3e8d52679bcb4f9eed3f484356680b4b519872
Jan Janech <jan.janech@kst.uniza.sk>
2021-11-26 08:51:14 +0100

    copying stylesheets from the original window

--- BlazorWindows/wwwroot/WindowHandler.js
+++ BlazorWindows/wwwroot/WindowHandler.js
@@ -1,7 +1,23 @@
 ﻿var windows = { };
 
 export function OpenWindow(id, content, windowFeatures) {
-    var win = window.open("about:blank", id, windowFeatures);
+    let win = window.open("about:blank", id, windowFeatures);
     windows[id] = win;
     win.document.body.appendChild(content);
+
+    let baseUrl = new URL(
+        document.head.querySelector("base").getAttribute("href"),
+        document.location.href
+    );
+
+    let baseElement = win.document.createElement("base");
+    baseElement.setAttribute("href", baseUrl.href);
+    win.document.head.appendChild(baseElement);
+
+    document.head
+        .querySelectorAll("link[rel='stylesheet']")
+        .forEach(function(item) {
+            win.document.head.appendChild(item.cloneNode());
+        });
+
 }
