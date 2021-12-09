5223e54b9a9940de44bd3081460fa8792ae97f2f
Jan Janech <jan.janech@kst.uniza.sk>
2021-12-09 07:54:52 +0100

    possibility to attach event callback to window child component

--- BlazorWindows/ComponentParameterBag.cs
+++ BlazorWindows/ComponentParameterBag.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace KST.Blazor.Windows
 {
@@ -33,6 +35,42 @@ namespace KST.Blazor.Windows
 		public IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, TValue>> parameter, TValue value)
 		{
 			this.aProperties[((MemberExpression)parameter.Body).Member.Name] = value;
+			return this;
+		}
+
+		public IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Action callback)
+		{
+			this.aProperties[((MemberExpression)parameter.Body).Member.Name] = new EventCallback<TValue>(callback.Target as IHandleEvent, callback);
+			return this;
+		}
+
+		public IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Action<TValue> callback)
+		{
+			this.aProperties[((MemberExpression)parameter.Body).Member.Name] = new EventCallback<TValue>(callback.Target as IHandleEvent, callback);
+			return this;
+		}
+
+		public IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Func<Task> callback)
+		{
+			this.aProperties[((MemberExpression)parameter.Body).Member.Name] = new EventCallback<TValue>(callback.Target as IHandleEvent, callback);
+			return this;
+		}
+
+		public IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Func<TValue, Task> callback)
+		{
+			this.aProperties[((MemberExpression)parameter.Body).Member.Name] = new EventCallback<TValue>(callback.Target as IHandleEvent, callback);
+			return this;
+		}
+
+		public IComponentParameterBag<TComponent> Set(Expression<Func<TComponent, EventCallback>> parameter, Action callback)
+		{
+			this.aProperties[((MemberExpression)parameter.Body).Member.Name] = new EventCallback(callback.Target as IHandleEvent, callback);
+			return this;
+		}
+
+		public IComponentParameterBag<TComponent> Set(Expression<Func<TComponent, EventCallback>> parameter, Func<Task> callback)
+		{
+			this.aProperties[((MemberExpression)parameter.Body).Member.Name] = new EventCallback(callback.Target as IHandleEvent, callback);
 			return this;
 		}
 
--- BlazorWindows/IComponentParameterBag.cs
+++ BlazorWindows/IComponentParameterBag.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace KST.Blazor.Windows
 {
 	public interface IComponentParameterBag<TComponent>
 	{
 		IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, TValue>> parameter, TValue value);
+
+		IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Action callback);
+		IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Action<TValue> callback);
+		IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Func<Task> callback);
+		IComponentParameterBag<TComponent> Set<TValue>(Expression<Func<TComponent, EventCallback<TValue>>> parameter, Func<TValue, Task> callback);
+		IComponentParameterBag<TComponent> Set(Expression<Func<TComponent, EventCallback>> parameter, Action callback);
+		IComponentParameterBag<TComponent> Set(Expression<Func<TComponent, EventCallback>> parameter, Func<Task> callback);
 	}
 }
